4d4a368bc31f94bee9170cc90b08a0428ce132b9
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-04-09 22:30:17 +0200

    Implement airplane event.

---
+++ EventArgs/AirplaneEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ControlTower
+{
+    public class AirplaneEventArgs
+    {
+        public string FlightNumber { get; }
+        public string Message { get; }
+        public DateTime Timestamp { get; }
+
+        public AirplaneEventArgs(string flightNumber, string message)
+        {
+            FlightNumber = flightNumber;
+            Message = message;
+            Timestamp = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"{Timestamp:HH:mm:ss} - {FlightNumber}: {Message}";
+        }
+    }
+}
