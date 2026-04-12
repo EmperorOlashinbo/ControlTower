e48f96af782fb2c06c8888f28e5b91bc77f00ae0
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-04-12 19:41:16 +0200

    Implementing main form.

--- MainForm.cs
+++ MainForm.cs
@@ -10,11 +10,29 @@ using System.Windows.Forms;
 
 namespace ControlTower
 {
+    /// <summary>
+    /// Represents the main form of the Airport Simulator application, responsible for managing the user interface and interactions with the control tower,
+    /// including adding flights, authorizing takeoffs, changing altitudes, and removing flights.
+    /// </summary>
     public partial class MainForm : Form
     {
+        private readonly ControlTower tower = new ControlTower();
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainForm"/> class.
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
+
+            // Subscribe to tower events
+            tower.TakeOff += Tower_TakeOff;
+            tower.Landed += Tower_Landed;
+            tower.StatusUpdated += Tower_StatusUpdated;
+
+            UpdateListView();
         }
+
+        
     }
 }
