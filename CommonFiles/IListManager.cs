897a08b01b3f945a418bf63eb2e90dbb732489aa
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-04-13 22:37:25 +0200

    Implement list manager interface.

--- CommonFiles/IListManager.cs
+++ CommonFiles/IListManager.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+
 
 namespace ControlTower.CommonFiles
 {
-    internal class IListManager
+    /// <summary>
+    /// Lightweight generic list manager interface used by the ControlTower project.
+    /// Provides basic collection operations and enumeration.
+    /// </summary>
+    /// <typeparam name="T">Item type.</typeparam>
+    public interface IListManager<T> : IEnumerable<T>
     {
+        /// <summary>
+        /// Adds an item to the collection.
+        /// </summary>
+        void Add(T item);
+
+        /// <summary>
+        /// Removes the item at the given index. Returns true if removed; false if index invalid.
+        /// </summary>
+        bool RemoveAt(int index);
+
+        /// <summary>
+        /// Clears the collection.
+        /// </summary>
+        void Clear();
+
+        /// <summary>
+        /// Gets the number of items.
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// Indexer to get item by index. Throws if index invalid.
+        /// </summary>
+        T this[int index] { get; }
     }
 }
